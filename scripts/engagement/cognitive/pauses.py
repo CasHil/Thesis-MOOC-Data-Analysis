a0fe4d2a8cbe778057f4e20c20aed3df852e0980
Casper Hildebrand <casperhildebrand@gmail.com>
2024-02-27 00:21:05 +0100

    Calculate correlation between preference for videos and pause events, and perform regression with interaction between preference, pause events and gender

--- scripts/engagement/cognitive/pauses.py
+++ scripts/engagement/cognitive/pauses.py
@@ -5,7 +5,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os
 import pandas as pd
-from scipy.stats import shapiro, levene, ttest_ind, mannwhitneyu
+from scipy.stats import shapiro, levene, ttest_ind, mannwhitneyu, spearmanr
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
 
 WORKING_DIR = 'W:/staff-umbrella/gdicsmoocs/Working copy'
 DB_LOCATION = WORKING_DIR + '/scripts/thesis_db'
@@ -13,6 +15,9 @@ COURSES = ['EX101x', 'ST1x', 'UnixTx']
 
 def find_all_log_files_for_course_id(course_id):
     return glob.glob(f"{WORKING_DIR}/{course_id}*/*.log", recursive=True)
+
+def find_presurvey_files_for_course_id(course_id):
+    return glob.glob(f"{WORKING_DIR}/{course_id}*/pre_survey*.txt", recursive=True)
 
 def find_all_pause_events_for_course_id(course_id):
     pause_events = []
@@ -23,10 +28,31 @@ def find_all_pause_events_for_course_id(course_id):
                     pause_events.append(line)
     return pause_events
 
-def calculate_average_pause_count_per_gender(course_id):
+# def find_all_pause_events_for_course_id_limit_5(course_id):
+#     pause_events = []
+#     for i, log_file in enumerate(find_all_log_files_for_course_id(course_id)):
+#         if i == 5:
+#             break
+#         with open(log_file, 'r', encoding='utf-8') as f:
+#             for line in f:
+#                 if 'pause_video' in line:
+#                     pause_events.append(line)
+#     return pause_events
+
+def find_gender_by_user_id(user_id):
     con = sqlite3.connect(DB_LOCATION)
     cur = con.cursor()
-    pause_events = find_all_pause_events_for_course_id(course_id)
+    query = 'SELECT gender FROM user_profiles WHERE hash_id = ?'
+    cur.execute(query, (user_id,))
+    result = cur.fetchone()
+    if result:
+        return result[0]
+    return None
+
+def calculate_pause_counts(course_id):
+    con = sqlite3.connect(DB_LOCATION)
+    cur = con.cursor()
+    pause_events = find_all_pause_events_for_course_id_limit_5(course_id)
 
     user_pause_counts = {}
     for pause_event in pause_events:
@@ -40,11 +66,7 @@ def calculate_average_pause_count_per_gender(course_id):
 
     gender_pause_counts = {'m': [], 'f': [], 'o': [], None: []}
     for user_id, pause_count in user_pause_counts.items():
-        query = 'SELECT gender FROM user_profiles WHERE hash_id = ?'
-        cur.execute(query, (user_id,))
-        result = cur.fetchone()
-        if result:
-            gender = result[0]
+            gender = find_gender_by_user_id(user_id)
             gender_pause_counts[gender].append(pause_count)
 
     gender_pause_averages = {}
@@ -56,7 +78,7 @@ def calculate_average_pause_count_per_gender(course_id):
     cur.close()
     con.close()
 
-    return gender_pause_counts, gender_pause_averages
+    return gender_pause_counts, gender_pause_averages, user_pause_counts
 
 def graph_average_pauses_per_gender(average_pauses_df):
     if not os.path.exists('graphs'):
@@ -99,6 +121,71 @@ def create_pause_count_df(pause_counts, course_id):
     df['Gender'] = df['Gender'].map(gender_map)
     return df
 
+def create_user_id_pause_count(pause_counts, course_id):
+    rows = []
+    for user, count in pause_counts.items():
+        gender = find_gender_by_user_id(user)
+        rows.append({"Course": course_id, "User": user, "Gender": gender, "PauseCount": count})
+    df = pd.DataFrame(rows)
+    
+    gender_map = {'m': 'Male', 'f': 'Female', 'o': 'Other', None: 'Prefer not to say / Unknown'}
+    df['Gender'] = df['Gender'].map(gender_map)
+    
+    pre_survey_accumulated = pd.DataFrame()
+
+    for pre_survey_file in find_presurvey_files_for_course_id(course_id):
+        pre_survey = pd.read_csv(pre_survey_file, sep='\t')
+        pre_survey = pre_survey[['hash_id', 'Q108_1']].dropna()
+        
+        pre_survey_accumulated = pd.concat([pre_survey_accumulated, pre_survey], ignore_index=True)
+    
+    pre_survey_accumulated = pre_survey_accumulated.drop_duplicates(subset=['hash_id'])
+    
+    df = df.merge(pre_survey_accumulated, left_on='User', right_on='hash_id', how='inner')
+    return df
+
+def test_correlation_between_video_preferences_and_pauses(df):
+    POSSIBLE_ANSWERS = {
+        'Not at all important': 1,
+        'Slightly important': 2,
+        'Moderately important': 3,
+        'Important': 4,
+        'Very important': 5,
+        'Extremely important': 6
+    }
+    
+    df['Q108_1'] = df['Q108_1'].astype(str)
+    df['Q108_1_numeric'] = df['Q108_1'].map(POSSIBLE_ANSWERS)
+    
+    correlation, p_value = spearmanr(df['Q108_1_numeric'], df['PauseCount'])
+    
+    return correlation, p_value
+
+def regression_with_interaction(df):
+    df['Q108_1_numeric'] = df['Q108_1'].map({
+        'Not at all important': 1,
+        'Slightly important': 2,
+        'Moderately important': 3,
+        'Important': 4,
+        'Very important': 5,
+        'Extremely important': 6
+    })
+    
+    if df['Gender'].dtype == 'object':
+        df['Gender_code'] = df['Gender'].astype('category').cat.codes
+    else:
+        df['Gender_code'] = df['Gender']
+    
+    # Fit the regression model with an interaction term between Q108_1_numeric and Gender
+    model = smf.ols('PauseCount ~ Q108_1_numeric * Gender_code', data=df).fit()
+    return model.summary()
+
+def descriptive_statistics(df):
+    desc_stats = df.groupby(['Gender'])['PauseCount'].describe(percentiles=[.25, .5, .75])
+    desc_stats['IQR'] = desc_stats['75%'] - desc_stats['25%']
+    desc_stats['Variance'] = df.groupby(['Gender'])['PauseCount'].var() 
+    return desc_stats
+
 def test_normality(df, value_col):
     p_values = {}
     for group in df['Gender'].unique():
@@ -132,17 +219,27 @@ def main():
 
     for course_id in COURSES:
         print(f"Analysing pauses in course: {course_id}")
-        gender_pause_counts, gender_pause_averages = calculate_average_pause_count_per_gender(course_id)
+        gender_pause_counts, gender_pause_averages, user_pause_counts = calculate_pause_counts(course_id)
         gender_pause_counts_per_course[course_id] = gender_pause_counts
         average_pauses_per_course[course_id] = gender_pause_averages
+        
+        user_pause_df = create_user_id_pause_count(user_pause_counts, course_id)
+        correlation, p_value = test_correlation_between_video_preferences_and_pauses(user_pause_df)
+        print("Correlation between video preferences and pauses:", correlation)
+        print("P-Value:", p_value)
+        print("---------------------------------------------")
+        print("Regression with Interaction Results:")
+        print(regression_with_interaction(user_pause_df))
+        print("---------------------------------------------")
         pause_count_df = create_pause_count_df(gender_pause_counts_per_course[course_id], course_id)
         pause_count_df = pause_count_df.loc[pause_count_df['Gender'].isin(['Male', 'Female'])]
-        
+        stats = descriptive_statistics(pause_count_df)
+        print(stats)
         print("Normality Test Results:", test_normality(pause_count_df, 'PauseCount'))
         print("Homogeneity of Variances Test P-Value:", test_homogeneity_of_variances(pause_count_df, 'PauseCount'))
         print("T-Test P-Value between Male and Female:", perform_ttest(pause_count_df, 'PauseCount', 'Male', 'Female'))
         print("Mann-Whitney U Test P-Value between Male and Female:", perform_mannwhitney(pause_count_df, 'PauseCount', 'Male', 'Female'))
-
+        print("=============================================")
     average_pause_df = create_average_pause_df(average_pauses_per_course)
     graph_average_pauses_per_gender(average_pause_df)
 
---
+++ scripts/engagement/presurvey/presurvey.py
@@ -0,0 +1,24 @@
+import pandas as pd
+
+WORKING_DIR = 'W:/staff-umbrella/gdicsmoocs/Working copy'
+PRESURVEY = WORKING_DIR + '/EX101x_2T2018_run6/pre_survey_results_EX101x_2T2018.txt'
+
+def main():
+    # Read the presurvey file. It is a TSV but in a txt format. Print the first 10 lines in table format.
+    presurvey = pd.read_csv(PRESURVEY, sep='\t')
+    # I want to see Q108
+    print(presurvey['Q108_1'].value_counts())
+    # Drop StartDate and EndDate
+    # presurvey = presurvey.drop(columns=['StartDate', 'EndDate', 'Status', 'Progress', 'Duration..in.seconds.', 'Finished', 'RecordedDate', 'ResponseId', 'DistributionChannel', 'hash_id', 'course_id', 'course_type', 'Q4.2', 'Q4.2.4_1', 'Q4.2.4_2', 'Q4.3_1', 'Q4.4', 'Q4.5_1', 'Q4.5_2', 'Q4.6_9_TEXT', 'SP'])
+    print(presurvey.head(10))
+
+    # Q4.2: Which of the following best describes your current employment status?
+    # Q4.3_1: What is your age? - Years:
+    # Q4.2.4_1: In which industry do you currently work - Sector
+    # Q4.4: What is your gender?
+    # Q4.5_1: What is your (first) nationality (continent)
+    # Q4.5_2: What is your (first) nationality (continent)?
+    # Q4.6_9_TEXT: What is the highest degree or level of education you have completed?
+
+if __name__ == "__main__":
+    main()
