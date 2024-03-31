371360268e6c67f2e26c02e2ac6853c1c14fbacd
Casper Hildebrand <casperhildebrand@gmail.com>
2024-03-30 23:07:47 -1000

    Update MOOC schema to make it possible for log files to be added

--- scripts/db/mooc_db_schema.sql
+++ scripts/db/mooc_db_schema.sql
@@ -6,7 +6,7 @@ CREATE TABLE certificates (
     created_date DATETIME NOT NULL,
     modified_date DATETIME NOT NULL,
     mode VARCHAR(50),
-    PRIMARY KEY (hash_id, course_id)
+    PRIMARY KEY (hash_id, course_id),
     FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
@@ -15,7 +15,7 @@ CREATE TABLE course_instructors (
     org VARCHAR(255) NOT NULL,
     course_id VARCHAR(255) NOT NULL,
     role VARCHAR(100) NOT NULL,
-    PRIMARY KEY (hash_id, course_id)
+    PRIMARY KEY (hash_id, course_id, role)
 );
 
 CREATE TABLE enrollments (
@@ -24,7 +24,7 @@ CREATE TABLE enrollments (
     created DATETIME NOT NULL,
     is_active BOOLEAN NOT NULL,
     mode VARCHAR(50) NOT NULL,
-    PRIMARY KEY (hash_id, course_id)
+    PRIMARY KEY (hash_id, course_id),
     FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
@@ -34,9 +34,9 @@ CREATE TABLE grades (
     percent_grade NUMERIC(5,2),
     letter_grade VARCHAR(100),
     passed_timestamp DATETIME,
-    created DATETIME NOT NULL,
-    modified DATETIME NOT NULL,
-    PRIMARY KEY (hash_id, course_id)
+    created DATETIME,
+    modified DATETIME,
+    PRIMARY KEY (hash_id, course_id),
     FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
@@ -53,27 +53,27 @@ CREATE TABLE user_profiles (
 
 CREATE TABLE sessions (
     session_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
     start_time TEXT,
     end_time TEXT,
     duration NUMERIC,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
 CREATE TABLE forum_sessions (
     session_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
     times_search INTEGER,
     start_time TEXT,
     end_time TEXT,
     duration NUMERIC,
     relevent_element_id TEXT,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
 CREATE TABLE video_interactions (
     interaction_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id  NOT NULL,
     video_id TEXT NOT NULL,
     duration NUMERIC,
     times_forward_seek INTEGER,
@@ -86,38 +86,38 @@ CREATE TABLE video_interactions (
     duration_pause NUMERIC,
     start_time TEXT,
     end_time TEXT,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
 CREATE TABLE assessments (
     assessment_id TEXT PRIMARY KEY,
-    hash_id TEXT,
+    hash_id VARCHAR(255),
     max_grade NUMERIC,
     grade NUMERIC,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
 CREATE TABLE submissions (
     submission_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
     question_id TEXT NOT NULL,
     submission_timestamp TEXT,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id),
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id),
     FOREIGN KEY (question_id) REFERENCES quiz_questions(question_id)
 );
 
 CREATE TABLE quiz_sessions (
     session_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
     start_time TEXT,
     end_time TEXT,
     duration NUMERIC,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
 );
 
 CREATE TABLE ora_sessions (
     session_id TEXT PRIMARY KEY,
-    hash_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
     times_save INTEGER,
     times_peer_assess INTEGER,
     submitted BOOLEAN,
@@ -126,16 +126,8 @@ CREATE TABLE ora_sessions (
     end_time TEXT,
     duration NUMERIC,
     assessment_id TEXT,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id),
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id),
     FOREIGN KEY (assessment_id) REFERENCES assessments(assessment_id)
-);
-
-CREATE TABLE assessments (
-    assessment_id TEXT PRIMARY KEY,
-    hash_id TEXT,
-    max_grade NUMERIC,
-    grade NUMERIC,
-    FOREIGN KEY (hash_id) REFERENCES course_learner(hash_id)
 );
 
 CREATE TABLE quiz_questions (
@@ -145,3 +137,83 @@ CREATE TABLE quiz_questions (
     question_due TEXT
 );
 
+CREATE TABLE courses (
+    course_id TEXT PRIMARY KEY,
+    course_name TEXT,
+    start_time TEXT,
+    end_time TEXT
+);
+
+CREATE TABLE course_learner (
+    hash_id VARCHAR(255) PRIMARY KEY,
+    final_grade NUMERIC,
+    enrollment_mode TEXT,
+    certificate_status TEXT,
+    register_time TEXT,
+    group_type TEXT,
+    group_name TEXT,
+    segment TEXT,
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
+);
+
+CREATE TABLE learner_demographic (
+    hash_id VARCHAR(255) PRIMARY KEY,
+    gender TEXT,
+    year_of_birth INTEGER,
+    level_of_education TEXT,
+    country TEXT,
+    email TEXT,
+    segment TEXT,
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
+);
+
+CREATE TABLE course_elements (
+    element_id TEXT PRIMARY KEY,
+    element_type TEXT,
+    week INTEGER,
+    course_id TEXT,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+CREATE TABLE learner_index (
+    global_learner_id TEXT PRIMARY KEY,
+    course_id TEXT NOT NULL,
+    hash_id VARCHAR(255) NOT NULL,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id),
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
+);
+
+CREATE TABLE metadata (
+    course_id TEXT PRIMARY KEY,
+    object TEXT,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+CREATE TABLE forum_interaction (
+    post_id TEXT PRIMARY KEY,
+    hash_id VARCHAR(255),
+    post_type TEXT,
+    post_title TEXT,
+    post_content TEXT,
+    post_timestamp TEXT,
+    post_parent_id TEXT,
+    post_thread_id TEXT,
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id)
+);
+
+CREATE TABLE survey_descriptions (
+    question_id TEXT PRIMARY KEY,
+    course_id TEXT,
+    question_type TEXT,
+    question_description TEXT,
+    FOREIGN KEY (course_id) REFERENCES courses(course_id)
+);
+
+CREATE TABLE survey_responses (
+    response_id TEXT PRIMARY KEY,
+    hash_id VARCHAR(255),
+    question_id TEXT,
+    answer TEXT,
+    FOREIGN KEY (hash_id) REFERENCES user_profiles(hash_id),
+    FOREIGN KEY (question_id) REFERENCES survey_descriptions(question_id)
+);
